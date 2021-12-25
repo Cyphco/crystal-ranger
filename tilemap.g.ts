aedfef053578483311ce8c62e6ef5f3c108287c3
Cyphco <54905884+Cyphco@users.noreply.github.com>
2021-12-25 12:22:08 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ tilemap.g.ts
@@ -0,0 +1,15 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
